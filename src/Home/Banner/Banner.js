d05d18222d9178fc72366ff1e9aad0345eeb28c6
Rabi-Mezan <ravimezan@gmail.com>
2021-11-10 22:57:24 +0600

    header banner added

---
+++ src/Home/Banner/Banner.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+
+            <main class="dark:bg-gray-800 bg-white relative overflow-hidden ">
+
+                <div class="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
+                    <div class="container mx-auto px-6 flex relative py-16">
+                        <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+                            <span class="w-20 h-2 bg-gray-800 dark:bg-white mb-12">
+                            </span>
+                            <h1 class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
+                                Buy A
+                                <span class="text-5xl sm:text-7xl">
+                                    Dream
+                                </span>
+                            </h1>
+                            <p class="text-sm sm:text-base text-gray-700 dark:text-white my-4">
+                                Dimension of reality that makes change possible and understandable. An indefinite and homogeneous environment in which natural events and human existence take place.
+                            </p>
+                            <div class="flex  m-auto">
+                                <a href="/" class="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white text-md mr-4 hover:bg-blue-500">
+                                    Shop Now
+                                </a>
+                                <a href="/" class="uppercase py-2 px-4 rounded-lg bg-transparent border-2  text-pink-500 dark:text-white hover:bg-blue-500 hover:text-white text-md">
+
+                                    Learn More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative ml-20">
+                            <img className='lg:w-2/3' src="https://purepng.com/public/uploads/large/nikon-camera-ato.png" alt='' />
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/Group 1.png'
+
+const Header = () => {
+    return (
+        <div>
+
+
+            <div>
+                <nav class="bg-gray-700 dark:bg-gray-800  shadow py-4 ">
+                    <div class="max-w-7xl mx-auto px-8">
+                        <div class="flex items-center justify-between h-16">
+                            <div class=" flex items-center">
+                                <Link class="flex-shrink-0" to='/home'>
+                                    <img class="lg: w-1/3" src={logo} alt="Workflow" />
+                                </Link>
+                                <div class="hidden md:block">
+                                    <div class="ml-10 flex items-baseline space-x-4">
+                                        <Link class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" to='/home'>
+                                            Home
+                                        </Link>
+                                        <Link class="text-gray-300 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" to='/shopNow'>
+                                            Shop Now
+                                        </Link>
+
+                                        <Link class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" to='/contact'>
+                                            Contact
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block">
+                                <div class="ml-4 flex items-center md:ml-6">
+                                    <div class="ml-3 relative">
+                                        <div class="relative inline-block text-left">
+                                            <div>
+                                                <button type="button" class="  flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium
+                                                bg-white hover:bg-gray-300 " id="options-menu">
+                                                    Login
+                                                </button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="-mr-2 flex md:hidden">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:hidden">
+                        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            <Link class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/home'>
+                                Home
+                            </Link>
+                            <Link class="text-gray-300 dark:text-white block px-3 py-2 rounded-md text-base font-medium" to='/shopNow'>
+                                Shop Now
+                            </Link>
+
+                            <Link class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" to='/contact'>
+                                Contact
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+        </div >
+    );
+};
+
+export default Header;
